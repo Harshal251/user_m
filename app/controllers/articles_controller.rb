c429f54f415fa238f35c5d245b301463aa2d8022
harshal251 <harshal@webkorps.com>
2022-08-03 00:04:44 +0530

    crud

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,23 +1,43 @@
 class ArticlesController < ApplicationController
-  def show 
-  @article = Article.find(params[:id])
-  end
-    def index
+  def index
     @articles = Article.all
-    end
+  end
 
-      def new
-       
-      @article = Article.new 
-      end 
+  def show
+    @article = Article.find(params[:id])
+  end
 
-      def create
-       
-      @article = Article.new(params.require(:article).permit(:title, :discription))
-      if @article.save
-        redirect_to @article
-      else
-        render :new, status: :unprocessable_entity             #flashh 
-      end
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :discription))
+    if @article.save
+      flash[:notice] = "Article was created Successfully!!"
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity             #flashh
     end
-end  
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :discription))
+      flash[:notice] = "Article was updated successfully!!"
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to article_path
+  end
+end
