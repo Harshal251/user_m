52ab33a09cecb98169ff42161332af4df9304d84
harshal251 <harshal@webkorps.com>
2022-08-03 02:19:55 +0530

    dry actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :get_article, only: [:show, :destroy, :update, :edit]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
-  
   end
 
   def new
@@ -13,11 +13,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :discription))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was created Successfully!!"
       redirect_to @article
@@ -27,8 +26,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :discription))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully!!"
       redirect_to @article
     else
@@ -37,9 +35,19 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path, status: :see_other
      flash[:notice] = "Article was Deleted successfully!!"
   end
+
+  private
+
+  def get_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :discription)
+  end
+
 end
