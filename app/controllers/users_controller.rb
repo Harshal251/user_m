84c9fc23faeef7cc5b9c91c8be8d12023486740c
harshal251 <harshal@webkorps.com>
2022-08-04 21:14:21 +0530

    added signup in home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
-
   def new
     @user = User.new
   end
 
-
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "WELCOME #{@user.username} !! YOU HAVE SUCCESSFULLY SIGNED-UP!!"
+      redirect_to @user
+    else
+      render :new, status: :unprocessable_entity             #flashh
+    end
+  end
+  private
+    def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
-
